d50ffaf58bda3bf660e7a00f878557339f32df96
Tal.M <t.m.mushkin@gmail.com>
2025-07-13 13:43:59 +0300

    commented out imge

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,11 @@ export default function App() {
   return (
     <main>
       {/* Gradient image */}
-      <img
+      {/* <img
         className="absolute top-0 right-0 opacity-60 -z-10"
         src="/gradient.png"
         alt="Gradient-img"
-      />
+      /> */}
 
       {/* Blure Effect */}
       <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#ffffff] -rotate-[30deg] -z-10"></div>
